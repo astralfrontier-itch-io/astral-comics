c2c2c34b2e80566dca7a50906f530aaf519faa01
Bill Garrett <garrett@astralfrontier.org>
2021-04-23 22:52:14 -0700

    FIX: update build scripts

--- build.sh
+++ build.sh
@@ -16,15 +16,13 @@ function runPandoc() {
     local OUTPUT_BASE=${INPUT##*/}
     local OUTPUT=dist/$2${OUTPUT_BASE%.md}.pdf
     local TEMPLATE=${3:-default}
-    $PANDOC "$INPUT"                                        \
-        -o "$OUTPUT"                                        \
-        -M current_date="$(date)"                           \
-	    -M git_branch="$(git symbolic-ref --short HEAD)"    \
-        --data-dir pandoc                                   \
-        --template "$TEMPLATE"                              \
-        --lua-filter=filters/standard.lua                   \
-        --pdf-engine=xelatex                                \
-        -f markdown                                         \
+    $PANDOC "$INPUT"                        \
+        -o "$OUTPUT"                        \
+        --data-dir pandoc                   \
+        --template "$TEMPLATE"              \
+        --lua-filter=filters/standard.lua   \
+        --pdf-engine=xelatex                \
+        -f markdown                         \
         # 
 }
 
